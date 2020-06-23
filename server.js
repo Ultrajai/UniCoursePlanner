66eeb526317ade2324d337c5228ae0da9334eecc
ajai <ajai@uoguelph.ca>
2020-06-23 17:27:06 -0400

    Added code to scrape data from guelph sites.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const puppeteer = require('puppeteer')
 var mongoose = require('mongoose');
 
 //set our port
@@ -10,6 +11,91 @@ const port = 3000
 //config files
 var db = require('./config/db');
 mongoose.connect(db.url);
+
+// update dbs
+
+async function scrapeCourses(url){
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  var courses = [];
+  var j = 2;
+
+  while(true){
+
+    var course = {Code: '', Title: '', Availability: '', Description: '', Offerings: '', Prerequisites: '', Restrictions: ''}
+
+    const [el] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr[1]/th/a');
+
+    if(el == null){
+      break;
+    }
+
+    const title = await el.getProperty('textContent');
+    const rawTitle = await title.jsonValue();
+
+    const [el2] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr[2]/td');
+    const desc = await el2.getProperty('textContent');
+    const rawDesc = await desc.jsonValue();
+
+    var i = 3;
+
+    while (true) {
+
+      const [el3] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/th');
+
+      if(el3 == null){
+        break;
+      }
+
+      const section = await el3.getProperty('textContent');
+      const rawSection = await section.jsonValue();
+
+      if(rawSection == 'Offering(s):'){
+        const [el4] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/td');
+        const offer = await el4.getProperty('textContent');
+        const rawOffer = await offer.jsonValue();
+
+        course.Offerings = rawOffer.trim();
+
+      }
+      else if (rawSection == 'Restriction(s):') {
+        const [el4] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/td');
+        const restrictions = await el4.getProperty('textContent');
+        const rawRestrictions = await restrictions.jsonValue();
+
+        course.Restrictions = rawRestrictions.trim();
+      }
+      else if(rawSection == 'Prerequisite(s):'){
+        const [el4] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/td');
+        const prereq = await el4.getProperty('textContent');
+        const rawPrereq = await prereq.jsonValue();
+
+        course.Prerequisites = rawPrereq.trim();
+      }
+
+      i += 1;
+    }
+
+    course.Code = rawTitle.slice(0, 8);
+    course.Title = rawTitle.slice(9, rawTitle.search(/ [SFWU,]{1,5} /));
+    course.Availability = rawTitle.slice(rawTitle.search(/[SFWU,]{1,5} /), rawTitle.search(/ \([0-9][-][0-9]\) /));
+    course.Description = rawDesc.trim().split("\n").join("").split(/[ ]{2,}/).join("");
+
+    courses.push(course);
+
+
+    j += 1;
+  }
+
+
+  console.log(courses)
+
+
+}
+
+scrapeCourses('https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/c12cis.shtml');
 
 //front end routes
 app.get('/', function(req, res) {
