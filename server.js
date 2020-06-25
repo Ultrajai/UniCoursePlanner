12db1bf132f28dd3b6c0590698100a5d0b6101a4
ajai <ajai@uoguelph.ca>
2020-06-25 00:06:25 -0400

    fixed some web scrapping code

--- server.js
+++ server.js
@@ -16,81 +16,77 @@ mongoose.connect(db.url);
 // update dbs
 
 var courses = [];
+var indexCourses = 0;
 
-async function scrapeCourses(url){
-  const browser = await puppeteer.launch();
+async function scrapeCourses(url, browser){
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, {timeout: 0, waitUntil: 'networkidle0'});
 
-  var j = 2;
+  console.log('Visiting ' + url);
 
-  while(true){
+  var courseList = await page.$$('.course');
 
-    var course = {Code: '', Title: '', Availability: '', Description: '', Offerings: '', Prerequisites: '', Restrictions: ''}
-    
-    const [el] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr[1]/th');
 
-    if(el == null){
-      break;
+  for (var i = 0; i < courseList.length; i++) {
+
+    courses.push({Code: '', Title: '', Availability: '', Credits: '',Description: '', Offerings: '', Prerequisites: '', Restrictions: '', Equates: ''});
+
+    var titles = await courseList[i].$('.title');
+    var descriptions = await courseList[i].$('.description');
+    var offerings = await courseList[i].$('.offering');
+    var restrictions = await courseList[i].$('.restrictions');
+    var prereqs = await courseList[i].$('.prereqs');
+    var equates = await courseList[i].$('.equates');
+
+    var text = await titles.getProperty('textContent');
+    var rawText = await text.jsonValue();
+    rawText = rawText.trim();
+
+    courses[indexCourses].Code = rawText.slice(0, rawText.search(/\s/));
+    courses[indexCourses].Title = rawText.slice(rawText.search(/\s/), rawText.search(/ [SFWU,]{1,5} /));
+    courses[indexCourses].Availability = rawText.slice(rawText.search(/[SFWU,]{1,5} /), rawText.search(/ \([0-9][-][0-9]\) /));
+    courses[indexCourses].Credits = rawText.slice(rawText.search(/[0-9][.][0-9][0-9]/), rawText.length - 1);
+
+    text = await descriptions.getProperty('textContent');
+    rawText = await text.jsonValue();
+    rawText = rawText.trim();
+
+    courses[indexCourses].Description = rawText.split("\n").join("").split(/[ ]{2,}/).join("");
+
+    if(offerings != null){
+      text = await offerings.getProperty('textContent');
+      rawText = await text.jsonValue();
+      rawText = rawText.trim();
+
+      courses[indexCourses].Offerings = rawText.split("\n").join("").split(/[ ]{2,}/).join("").slice(rawText.search(':') + 1, rawText.length);
     }
 
-    const title = await el.getProperty('textContent');
-    const rawTitle = await title.jsonValue();
+    if(restrictions != null){
+      text = await restrictions.getProperty('textContent');
+      rawText = await text.jsonValue();
+      rawText = rawText.trim();
 
-    const [el2] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr[2]/td');
-    const desc = await el2.getProperty('textContent');
-    const rawDesc = await desc.jsonValue();
-
-    var i = 3;
-
-    while (true) {
-
-      const [el3] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/th');
-
-      if(el3 == null){
-        break;
-      }
-
-      const section = await el3.getProperty('textContent');
-      const rawSection = await section.jsonValue();
-
-      if(rawSection == 'Offering(s):'){
-        const [el4] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/td');
-        const offer = await el4.getProperty('textContent');
-        const rawOffer = await offer.jsonValue();
-
-        course.Offerings = rawOffer.trim();
-
-      }
-      else if (rawSection == 'Restriction(s):') {
-        const [el4] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/td');
-        const restrictions = await el4.getProperty('textContent');
-        const rawRestrictions = await restrictions.jsonValue();
-
-        course.Restrictions = rawRestrictions.trim();
-      }
-      else if(rawSection == 'Prerequisite(s):'){
-        const [el4] = await page.$x('//*[@id="content"]/div['+ j +']/table/tbody/tr['+ i +']/td');
-        const prereq = await el4.getProperty('textContent');
-        const rawPrereq = await prereq.jsonValue();
-
-        course.Prerequisites = rawPrereq.trim();
-      }
-
-      i += 1;
+      courses[indexCourses].Restrictions = rawText.split("\n").join("").split(/[ ]{2,}/).join("").slice(rawText.search(':') + 1, rawText.length);
     }
 
-    course.Code = rawTitle.slice(0, 8);
-    course.Title = rawTitle.slice(9, rawTitle.search(/ [SFWU,]{1,5} /));
-    course.Availability = rawTitle.slice(rawTitle.search(/[SFWU,]{1,5} /), rawTitle.search(/ \([0-9][-][0-9]\) /));
-    course.Description = rawDesc.trim().split("\n").join("").split(/[ ]{2,}/).join("");
+    if(prereqs != null){
+      text = await prereqs.getProperty('textContent');
+      rawText = await text.jsonValue();
+      rawText = rawText.trim();
 
-    courses.push(course);
+      courses[indexCourses].Prerequisites = rawText.split("\n").join("").split(/[ ]{2,}/).join("").slice(rawText.search(':') + 1, rawText.length);
+    }
 
+    if(equates != null){
+      text = await equates.getProperty('textContent');
+      rawText = await text.jsonValue();
+      rawText = rawText.trim();
 
-    j += 1;
+      courses[indexCourses].Equates = rawText.split("\n").join("").split(/[ ]{2,}/).join("").slice(rawText.search(':') + 1, rawText.length);
+    }
+
+    indexCourses += 1;
   }
-
 
   console.log(courses)
 
@@ -98,21 +94,43 @@ async function scrapeCourses(url){
 }
 
 async function checkIfUpdated(url){
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+        headless : true,
+        devtools : false
+    });
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(url, {timeout: 0, waitUntil: 'networkidle0'});
 
   const [el] = await page.$x('//*[@id="content"]/div[1]/h1');
   const version = await el.getProperty('textContent');
   const rawVersion = await version.jsonValue();
 
-  if(db.Version != rawVersion){
+  if(db.Version != rawVersion.trim()){
     db.Version = rawVersion.trim();
 
-    fs.writeFile('./config/db.js', "module.exports = {url : 'mongodb://localhost:27017/test', Version: '"+ rawVersion.trim() +"'}", (err) => {if (err) throw err;});
+    //fs.writeFile('./config/db.js', "module.exports = {url : 'mongodb://localhost:27017/test', Version: '"+ rawVersion.trim() +"'}", (err) => {if (err) throw err;});
 
     console.log('Updating database....');
-    //scrapeCourses('https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/c12cis.shtml');
+    const page2 = await browser.newPage();
+    await page2.goto('https://www.uoguelph.ca/registrar/calendars/undergraduate/current/c12/index.shtml', {timeout: 0, waitUntil: 'networkidle0'});
+
+    var i = 1;
+    while (true) {
+      const [el2] = await page2.$x('//*[@id="sidebar"]/div/ul[2]/li['+ i +']/a');
+
+      if(el2 == null){
+        console.log("hehe")
+        break;
+      }
+
+      const link = await el2.getProperty('href');
+      const rawLink = await link.jsonValue();
+
+      await scrapeCourses(rawLink.toString(), browser);
+      i += 1;
+    }
+
+
   }
 
 }
