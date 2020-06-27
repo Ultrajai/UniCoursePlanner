658f0f6aa559f894f9c722167438794383afeec6
ajai <ajai@uoguelph.ca>
2020-06-27 14:10:05 -0400

    added some html elements and functionality to increment the value of the semester and obtain the correct season for the semester for the cards in the page.

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ UniCoursePlanner = angular.module("UniCoursePlanner", ["ngRoute"]);
 //   //this is the before the page even loads
 // });
 
-UniCoursePlanner.controller("TestController", function($scope){
+UniCoursePlanner.controller("MainController", function($scope){
+
+  $scope.numOfSemesters = 0;
+  $scope.season = ['Fall', 'Winter', 'Summer'];
+  $scope.semesters = [];
+
+  $scope.AddSemester = function(){
+    $scope.semesters.push({num : $scope.numOfSemesters, season : $scope.season[$scope.numOfSemesters % 3]});
+    $scope.numOfSemesters += 1;
+  };
 
   //this controls the body
 });
