5ed8ffd14cae368f9fe98b472856877684e2330e
ajai <ajai@uoguelph.ca>
2020-07-08 18:33:35 -0400

    Added some my own personal filter code to filter course based on Code and Title only. I also limited the results to 100 to improve performance...

--- app.js
+++ app.js
@@ -5,16 +5,48 @@ UniCoursePlanner = angular.module("UniCoursePlanner", ["ngRoute"]);
 //   //this is the before the page even loads
 // });
 
+UniCoursePlanner.filter('CourseFilter', function(){
+  // Just add arguments to your HTML separated by :
+  // And add them as parameters here, for example:
+  // return function(dataArray, searchTerm, argumentTwo, argumentThree) {
+  return function(dataArray, searchTerm) {
+      // If no array is given, exit.
+      if (!dataArray) {
+          return;
+      }
+      // If no search term exists, return the array unfiltered.
+      else if (!searchTerm) {
+          return;
+      }
+      // Otherwise, continue.
+      else {
+           // Convert filter text to lower case.
+           var term = searchTerm.toLowerCase();
+           // Return the array and filter it by looking for any occurrences of the search term in each items Code or Title
+           return dataArray.filter(function(item){
+              var termInCode = item.Code.toLowerCase().indexOf(term) > -1; // returns true if there is ansintance of the term
+              var termInTitle = item.Title.toLowerCase().indexOf(term) > -1; // returns true if there is ansintance of the term
+              return termInCode || termInTitle;
+           });
+      }
+  }
+});
+
 UniCoursePlanner.controller("MainController", function($scope, $http){
 
   $scope.courses = [];
   $scope.numOfSemesters = 0;
   $scope.season = ['Fall', 'Winter', 'Summer'];
   $scope.semesters = [];
+  $scope.searchString = "";
 
   $scope.AddSemester = function(){
     $scope.semesters.push({num : $scope.numOfSemesters, season : $scope.season[$scope.numOfSemesters % 3]});
     $scope.numOfSemesters += 1;
+  };
+
+  $scope.SearchCourses = function(){
+    $scope.searchString = $scope.searchText;
   };
 
   //get all courses so that the webapp has access to all data from startup
